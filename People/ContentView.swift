d35931e1eb233093fa548f3b25913264269ef34e
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-09-20 19:51:16 +0300

    Initial Commit

---
+++ People/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  People
+//
+//  Created by Amby on 20/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ People/PeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleApp.swift
+//  People
+//
+//  Created by Amby on 20/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct PeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
