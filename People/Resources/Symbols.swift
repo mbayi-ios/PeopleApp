e4c380330fc1eee380c6dc7988304d09270ef0c5
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-09-20 20:19:35 +0300

    setting up project - add resources(theme, sysmbols)

---
+++ People/Resources/Symbols.swift
@@ -0,0 +1,15 @@
+//
+//  Symbols.swift
+//  People
+//
+//  Created by Amby on 20/09/2022.
+//
+
+import Foundation
+import SwiftUI
+
+enum Symbols {
+    static let person = Image(systemName: "person.2")
+    static let gear = Image(systemName: "gear")
+    static let plus = Image(systemName: "plus")
+}
