ba97e2d61bbb20103a92bad1382b478c4aafdf1d
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-09-21 20:24:50 +0300

    how to send a post request to create a user

---
+++ People/Features/Create/Models/NewPerson.swift
@@ -0,0 +1,14 @@
+//
+//  NewPerson.swift
+//  People
+//
+//  Created by Amby on 21/09/2022.
+//
+
+import Foundation
+
+struct NewPerson: Codable {
+    var firstName: String = ""
+    var lastName: String = ""
+    var job: String = ""
+}
--- People/Features/Create/Views/CreateView.swift
+++ People/Features/Create/Views/CreateView.swift
@@ -8,7 +8,10 @@
 import SwiftUI
 
 struct CreateView: View {
+
     @Environment(\.dismiss) private var dismiss
+    @StateObject private var vm = CreateViewModel()
+
     var body: some View {
         NavigationView {
             Form {
@@ -23,6 +26,11 @@ struct CreateView: View {
             .toolbar {
                 ToolbarItem(placement: .primaryAction) {
                     done
+                }
+            }
+            .onChange(of: vm.state) { formState in
+                if formState == .successful {
+                    dismiss()
                 }
             }
         }
@@ -42,18 +50,18 @@ private extension CreateView {
         }
     }
     var firstName: some View {
-        TextField("First Name", text: .constant(""))
+        TextField("First Name", text: $vm.person.firstName)
     }
     var lastName: some View {
-        TextField("Last Name", text: .constant(""))
+        TextField("Last Name", text:$vm.person.lastName)
     }
     var job: some View {
-        TextField("Job", text: .constant(""))
+        TextField("Job", text: $vm.person.job)
     }
     
     var submit: some View {
         Button("Submit") {
-            
+            vm.create()
         }
     }
 }
