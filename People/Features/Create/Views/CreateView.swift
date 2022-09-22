cb1d7a999a069b6df9bc02e1ec2f3faccc5304ec
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-09-22 11:12:02 +0300

    form validation

--- People/Features/Create/Views/CreateView.swift
+++ People/Features/Create/Views/CreateView.swift
@@ -10,15 +10,24 @@ import SwiftUI
 struct CreateView: View {
 
     @Environment(\.dismiss) private var dismiss
+    @FocusState private var focusedField: Field?
     @StateObject private var vm = CreateViewModel()
     let successfulAction: () -> Void
 
     var body: some View {
         NavigationView {
             Form {
+                Section {
                 firstName
                 lastName
                 job
+                } footer:  {
+                    if case .validation(let err) = vm.error,
+                       let errorDesc = err.errorDescription {
+                        Text(errorDesc)
+                            .foregroundColor(.red)
+                    }
+                }
                 Section {
                     submit
                 }
@@ -46,6 +55,14 @@ struct CreateView: View {
     }
 }
 
+extension CreateView {
+    enum Field: Hashable {
+        case firstName
+        case lastName
+        case job
+    }
+}
+
 struct CreateView_Previews: PreviewProvider {
     static var previews: some View {
         CreateView {}
@@ -60,16 +77,20 @@ private extension CreateView {
     }
     var firstName: some View {
         TextField("First Name", text: $vm.person.firstName)
+            .focused($focusedField, equals: .firstName)
     }
     var lastName: some View {
         TextField("Last Name", text:$vm.person.lastName)
+            .focused($focusedField, equals: .lastName)
     }
     var job: some View {
         TextField("Job", text: $vm.person.job)
+            .focused($focusedField, equals: .job)
     }
     
     var submit: some View {
         Button("Submit") {
+            focusedField = nil
             vm.create()
         }
     }
